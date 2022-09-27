95840b50d3f9256aa8094e8f3e0e22616668e91e
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-09-26 21:18:11 -0400

    2:Mostrar posicion inicial  y ubicacion del auto dirigido

--- src/autoDirigido.js
+++ src/autoDirigido.js
@@ -2,10 +2,11 @@ class Auto {
   constructor() {
     this.posX = 0;
     this.posY = 0;
+    this.ubicacion = "N"
   }
 
   mover() {
-    return this.posX.toString() + ","+this.posY.toString();
+    return this.posX.toString() + ","+this.posY.toString()+this.ubicacion;
   }
 }
 
